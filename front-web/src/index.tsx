2da220ece2bd6657d6c03d410ad733035fc0c368
Igor Teixeira <igorxdev@gmail.com>
2021-02-04 10:39:41 -0300

    Add fron-web

---
+++ front-web/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+ReactDOM.render(
+   <App/>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
