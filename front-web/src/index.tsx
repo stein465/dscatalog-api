78055fab4b1b94b2ba16bb61dcf7c7615d41fabd
Igor Teixeira <igorxdev@gmail.com>
2021-02-08 10:09:56 -0300

    add navbar

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 
 
 
